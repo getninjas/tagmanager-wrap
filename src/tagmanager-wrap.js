165d5cb8a5aaf82d7d65c1f1e7cfe28552ed8eb1
Ion D. Filho <iondrimba@gmail.com>
2017-09-29 10:46:54 -0300

    added async script dynamically

--- src/tagmanager-wrap.js
+++ src/tagmanager-wrap.js
@@ -14,6 +14,8 @@ export default class TagManager {
     this.dataLayer = dataLayer || [];
     this.setupExperiments();
     this.dataLayer.push(this.options.startPush);
+
+    this.appendAsyncScript();
   }
 
   setupExperiments() {
@@ -22,6 +24,13 @@ export default class TagManager {
     });
 
     return this;
+  }
+
+  appendAsyncScript() {
+    const script = document.createElement('script');
+    script.innerHTML = `(function (w, d, s, l, i) { w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' }); var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f); })(window, document, 'script', 'tagManagerDataLayer', '${this.options.gtmId}');`;
+
+    document.body.appendChild(script);
   }
 
   experiments() {
