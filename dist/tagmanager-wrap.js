39f9b755e07ad3f6293e4446b57bee602fa51096
Ion D. Filho <iondrimba@gmail.com>
2017-09-29 10:43:15 -0300

    added tagmanager demo

--- dist/tagmanager-wrap.js
+++ dist/tagmanager-wrap.js
@@ -68,6 +68,7 @@
   }();
 
   var defaultOptions = {
+    gtmId: 'xxxx',
     virtualPageViewEvent: 'virtual_pageview',
     startPush: {
       experiements: []
@@ -85,6 +86,9 @@
       this.dataLayer = dataLayer || [];
       this.setupExperiments();
       this.dataLayer.push(this.options.startPush);
+
+      this.appendAsyncScript();
+      this.appendNoScriptFallBack();
     }
 
     _createClass(TagManager, [{
@@ -95,6 +99,26 @@
         });
 
         return this;
+      }
+    }, {
+      key: 'appendAsyncScript',
+      value: function appendAsyncScript() {
+        var script = document.createElement('script');
+        script.innerHTML = '(function (w, d, s, l, i) { w[l] = w[l] || []; w[l].push({ \'gtm.start\': new Date().getTime(), event: \'gtm.js\' }); var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != \'dataLayer\' ? \'&l=\' + l : \'\'; j.async = true; j.src = \'https://www.googletagmanager.com/gtm.js?id=\' + i + dl; f.parentNode.insertBefore(j, f); })(window, document, \'script\', \'tagManagerDataLayer\', \'' + this.options.gtmId + '\');';
+
+        document.body.appendChild(script);
+      }
+    }, {
+      key: 'appendNoScriptFallBack',
+      value: function appendNoScriptFallBack() {
+        var noScript = document.createElement('noscript');
+        var iframe = document.createElement('iframe');
+        iframe.src = 'https://www.googletagmanager.com/ns.html?id=' + this.options.gtmId;
+        iframe.height = 0;
+        iframe.width = 0;
+        iframe.style = 'display:none;visibility:hidden';
+        noScript.appendChild(iframe);
+        document.body.appendChild(noScript);
       }
     }, {
       key: 'experiments',
