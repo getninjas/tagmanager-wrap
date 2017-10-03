e24ea6738d32980dfac7d1dabe0b4fd9319918d1
Ion Drimba Filho <ion.drimba@getninjas.com.br>
2017-10-03 13:09:08 -0300

    build

--- dist/tagmanager-wrap.js
+++ dist/tagmanager-wrap.js
@@ -17,6 +17,21 @@
     value: true
   });
 
+  function _defineProperty(obj, key, value) {
+    if (key in obj) {
+      Object.defineProperty(obj, key, {
+        value: value,
+        enumerable: true,
+        configurable: true,
+        writable: true
+      });
+    } else {
+      obj[key] = value;
+    }
+
+    return obj;
+  }
+
   var _extends = Object.assign || function (target) {
     for (var i = 1; i < arguments.length; i++) {
       var source = arguments[i];
@@ -56,7 +71,7 @@
   }();
 
   var defaultOptions = {
-    gtmId: 'GTM-T2HGD', // homolog
+    gtmId: '111111',
     virtualPageViewEvent: 'virtual_pageview',
     startPush: {
       experiements: []
@@ -65,41 +80,22 @@
   };
 
   var TagManager = function () {
-    function TagManager(dataLayer) {
+    function TagManager() {
+      var dataLayer = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
       var params = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : defaultOptions;
 
       _classCallCheck(this, TagManager);
 
       this.options = _extends({}, defaultOptions, params);
-      this.dataLayer = dataLayer || [];
+      this.dataLayer = dataLayer;
       this.dataLayer.push(this.options.startPush);
     }
 
     _createClass(TagManager, [{
       key: 'init',
       value: function init() {
-        this.appendAsyncScript();
-        this.appendNoScriptFallBack();
-      }
-    }, {
-      key: 'appendAsyncScript',
-      value: function appendAsyncScript() {
-        var script = document.createElement('script');
-        script.innerHTML = '(function (w, d, s, l, i) { w[l] = w[l] || []; w[l].push({ \'gtm.start\': new Date().getTime(), event: \'gtm.js\' }); var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != \'dataLayer\' ? \'&l=\' + l : \'\'; j.async = true; j.src = \'https://www.googletagmanager.com/gtm.js?id=\' + i + dl; f.parentNode.insertBefore(j, f); })(window, document, \'script\', \'tagManagerDataLayer\', \'' + this.options.gtmId + '\');';
-
-        document.body.appendChild(script);
-      }
-    }, {
-      key: 'appendNoScriptFallBack',
-      value: function appendNoScriptFallBack() {
-        var noScript = document.createElement('noscript');
-        var iframe = document.createElement('iframe');
-        iframe.src = 'https://www.googletagmanager.com/ns.html?id=' + this.options.gtmId;
-        iframe.height = 0;
-        iframe.width = 0;
-        iframe.style = 'display:none;visibility:hidden';
-        noScript.appendChild(iframe);
-        document.body.appendChild(noScript);
+        this._appendAsyncScript();
+        this._appendNoScriptFallBack();
       }
     }, {
       key: 'prependExperiment',
@@ -108,19 +104,21 @@
       }
     }, {
       key: 'virtualPageView',
-      value: function virtualPageView(vpname, event) {
+      value: function virtualPageView(vpname, eventType) {
         this.dataLayer.push({
           vpname: vpname,
-          event: event || this.options.virtualPageViewEvent
+          event: eventType || this.options.virtualPageViewEvent
         });
       }
     }, {
       key: 'eventCategory',
-      value: function eventCategory(event, infos) {
+      value: function eventCategory(_eventCategory) {
+        var infos = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
         var categoryObj = _extends({
-          eventCategory: event,
+          eventCategory: _eventCategory,
           event: 'GAEvent'
-        }, infos || {});
+        }, infos);
 
         return this.dataLayer.push(categoryObj);
       }
@@ -136,25 +134,69 @@
 
         gtmElements.forEach(function (el) {
           if (!el.getAttribute('data-gtm-bind')) {
-            el.addEventListener('click', this.clickGAEvent.bind(this));
-            el.setAttribute('data-gtm-binded', true);
+            el.addEventListener('click', this._clickGAEvent.bind(this));
+            el.setAttribute('data-gtm-bind', true);
           }
         }, this);
 
         return gtmElements;
       }
     }, {
-      key: 'clickGAEvent',
-      value: function clickGAEvent(event) {
-        var el = event.curentTarget;
+      key: '_appendAsyncScript',
+      value: function _appendAsyncScript() {
+        var script = document.createElement('script');
+        script.setAttribute('data-id', 'google-tagmanager');
 
-        var category = el.getAttribute('data-gtm-category');
-        var eventAction = el.getAttribute('data-gtm-action') || null;
-        var eventLabel = el.getAttribute('data-gtm-label') || null;
-        var eventValue = el.getAttribute('data-gtm-value') || null;
-        var eventProperty = el.getAttribute('data-gtm-property') || null;
+        script.id = 'google-tagmanager';
+        script.innerHTML = '(function (w, d, s, l, i) { w[l] = w[l] || []; w[l].push({ \'gtm.start\': new Date().getTime(), event: \'gtm.js\' }); var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != \'dataLayer\' ? \'&l=\' + l : \'\'; j.async = true; j.src = \'https://www.googletagmanager.com/gtm.js?id=\' + i + dl; f.parentNode.insertBefore(j, f); })(window, document, \'script\', \'tagManagerDataLayer\', \'' + this.options.gtmId + '\');';
 
-        this.eventCategory(category, { eventAction: eventAction, eventLabel: eventLabel, eventValue: eventValue, eventProperty: eventProperty });
+        document.body.appendChild(script);
+      }
+    }, {
+      key: '_appendNoScriptFallBack',
+      value: function _appendNoScriptFallBack() {
+        var noScript = document.createElement('noscript');
+        noScript.setAttribute('data-id', 'noscript-google-tagmanager');
+
+        var iframe = document.createElement('iframe');
+        iframe.src = 'https://www.googletagmanager.com/ns.html?id=' + this.options.gtmId;
+        iframe.height = 0;
+        iframe.width = 0;
+        iframe.style = 'display:none;visibility:hidden';
+        noScript.appendChild(iframe);
+        document.body.appendChild(noScript);
+      }
+    }, {
+      key: '_clickGAEvent',
+      value: function _clickGAEvent(evt) {
+        var el = evt.currentTarget;
+
+        var category = this._getAttribute(el, 'data-gtm-category');
+        var props = this._getProps(el, ['action', 'label', 'value', 'property', 'ddd']);
+
+        this.eventCategory(category, props);
+      }
+    }, {
+      key: '_getProps',
+      value: function _getProps(el, keys) {
+        var _this = this;
+
+        return keys.reduce(function (previousValue, currentValue) {
+          var key = 'event' + _this._captalize(currentValue);
+          var val = _this._getAttribute(el, 'data-gtm-' + currentValue);
+
+          return _extends({}, previousValue, _defineProperty({}, key, val));
+        }, {});
+      }
+    }, {
+      key: '_captalize',
+      value: function _captalize(word) {
+        return word && word[0].toUpperCase() + word.slice(1);
+      }
+    }, {
+      key: '_getAttribute',
+      value: function _getAttribute(el, attr) {
+        return el.getAttribute(attr) || null;
       }
     }]);
 
