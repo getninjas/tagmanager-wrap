f71d1f58ef92b386d0b9e93fa7fad52c0dbedc15
Ion Drimba Filho <ion.drimba@getninjas.com.br>
2017-10-03 13:08:52 -0300

    changed script tags tests

--- src/tagmanager-wrap.js
+++ src/tagmanager-wrap.js
@@ -59,6 +59,9 @@ export default class TagManager {
 
   _appendAsyncScript() {
     const script = document.createElement('script');
+    script.setAttribute('data-id', 'google-tagmanager');
+
+    script.id = 'google-tagmanager';
     script.innerHTML = `(function (w, d, s, l, i) { w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' }); var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f); })(window, document, 'script', 'tagManagerDataLayer', '${this.options.gtmId}');`;
 
     document.body.appendChild(script);
@@ -66,6 +69,8 @@ export default class TagManager {
 
   _appendNoScriptFallBack() {
     const noScript = document.createElement('noscript');
+    noScript.setAttribute('data-id', 'noscript-google-tagmanager');
+
     const iframe = document.createElement('iframe');
     iframe.src = `https://www.googletagmanager.com/ns.html?id=${this.options.gtmId}`;
     iframe.height = 0;
