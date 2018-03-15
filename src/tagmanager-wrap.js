c4bad0ce53bed95ed11ea5a518aa0b0a440c1f90
Ion Drimba Filho <ion.drimba@getninjas.com.br>
2018-03-15 14:22:40 -0300

    fixed prepend method

--- src/tagmanager-wrap.js
+++ src/tagmanager-wrap.js
@@ -21,11 +21,11 @@ export default class TagManager {
   }
 
   prependExperiment(experiment) {
-    var lastDataLayerElement = this.dataLayer[this.dataLayer.length - 1];
-    if (lastDataLayerElement.experiments) {
-      this.custom({ event: "split_test", experiments: lastDataLayerElement.experiments.push(experiment) });
+    if (this.options.startPush.experiments) {
+      this.options.startPush.experiments.push(experiment);
+      this.custom({ event: 'split_test', experiments: this.options.startPush.experiments });
     } else {
-      this.custom({ event: "split_test", experiments: [experiment] });
+      this.custom({ event: 'split_test', experiments: [experiment] });
     }
   }
 
@@ -70,6 +70,10 @@ export default class TagManager {
     script.innerHTML = `(function (w, d, s, l, i) { w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' }); var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f); })(window, document, 'script', 'tagManagerDataLayer', '${this.options.gtmId}');`;
 
     document.body.appendChild(script);
+
+    Object.assign(window.tagManagerDataLayer, this.dataLayer);
+
+    this.dataLayer = window.tagManagerDataLayer;
   }
 
   _appendNoScriptFallBack() {
