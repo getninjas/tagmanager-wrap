61be30972ccc8fae623ea9ab9c4cab1a5580f01d
Ion Drimba Filho <ion.drimba@getninjas.com.br>
2018-05-08 17:15:11 -0300

    dist files

--- dist/tagmanager-wrap.js
+++ dist/tagmanager-wrap.js
@@ -32,18 +32,6 @@
     return obj;
   }
 
-  function _toConsumableArray(arr) {
-    if (Array.isArray(arr)) {
-      for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
-        arr2[i] = arr[i];
-      }
-
-      return arr2;
-    } else {
-      return Array.from(arr);
-    }
-  }
-
   var _extends = Object.assign || function (target) {
     for (var i = 1; i < arguments.length; i++) {
       var source = arguments[i];
@@ -150,7 +138,7 @@
       value: function bindEvents() {
         var _this = this;
 
-        var gtmElements = [].concat(_toConsumableArray(document.querySelectorAll('[data-gtm-event="ga-event"]')));
+        var gtmElements = [].slice.call(document.querySelectorAll('[data-gtm-event="ga-event"]'));
 
         gtmElements.map(function (el) {
           if (!el.getAttribute('data-gtm-bind')) {
