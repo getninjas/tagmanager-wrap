8204d5bac40d3370c22e67f808e4bcb76c5b401e
wilfernandesjr <wilmar.wfsj@gmail.com>
2019-12-02 17:47:17 -0300

    🔥 Hotfix: generate builded file for production environment 🛠

--- dist/tagmanager-wrap.js
+++ dist/tagmanager-wrap.js
@@ -94,8 +94,8 @@
     _createClass(TagManager, [{
       key: 'init',
       value: function init() {
-        this._appendAsyncScript();
-        this._appendNoScriptFallBack();
+        _extends(window.tagManagerDataLayer, this.dataLayer);
+        this.dataLayer = window.tagManagerDataLayer;
       }
     }, {
       key: 'prependExperiment',
@@ -150,36 +150,6 @@
         });
 
         return gtmElements;
-      }
-    }, {
-      key: '_appendAsyncScript',
-      value: function _appendAsyncScript() {
-        var script = document.createElement('script');
-        script.setAttribute('data-id', 'google-tagmanager');
-
-        script.id = 'google-tagmanager';
-        script.innerHTML = '(function (w, d, s, l, i) { w[l] = w[l] || []; w[l].push({ \'gtm.start\': new Date().getTime(), event: \'gtm.js\' }); var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != \'dataLayer\' ? \'&l=\' + l : \'\'; j.async = true; j.src = \'https://www.googletagmanager.com/gtm.js?id=\' + i + dl; f.parentNode.insertBefore(j, f); })(window, document, \'script\', \'tagManagerDataLayer\', \'' + this.options.gtmId + '\');';
-
-        document.body.appendChild(script);
-
-        _extends(window.tagManagerDataLayer, this.dataLayer);
-
-        this.dataLayer = window.tagManagerDataLayer;
-      }
-    }, {
-      key: '_appendNoScriptFallBack',
-      value: function _appendNoScriptFallBack() {
-        var noScript = document.createElement('noscript');
-        noScript.setAttribute('data-id', 'noscript-google-tagmanager');
-
-        var iframe = document.createElement('iframe');
-        iframe.src = 'https://www.googletagmanager.com/ns.html?id=' + this.options.gtmId;
-        iframe.height = 0;
-        iframe.width = 0;
-        iframe.style.display = 'none';
-        iframe.style.visibility = 'hidden';
-        noScript.appendChild(iframe);
-        document.body.appendChild(noScript);
       }
     }, {
       key: '_clickGAEvent',
